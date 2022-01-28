5929137b4950bc0e178b045ee643b83eb9c98d27
Sreeraj S Chandran <sreeraj.s.c.vfc@gmail.com>
2022-01-28 20:51:33 +0530

    Display text clearing issue solved

--- src/Calculator.java
+++ src/Calculator.java
@@ -39,7 +39,7 @@ public class Calculator implements ActionListener
 		jf.setLayout(null);
 		jf.setSize(450, 650);
 		jf.setLocation(1070, 100);
-		displayLabel=new JLabel("Hello");
+		displayLabel=new JLabel();
 		displayLabel.setBounds(40, 40, 355, 40);
 		displayLabel.setBackground(Color.DARK_GRAY);
 		displayLabel.setOpaque(true);
@@ -54,77 +54,77 @@ public class Calculator implements ActionListener
 		eightButton=new JButton("8");
 		eightButton.setBounds(130, 130, 80, 80);
 		eightButton.setFont(new Font("Areal", Font.PLAIN, 40));
-		sevenButton.addActionListener(this);
+		eightButton.addActionListener(this);
 		jf.add(eightButton);
 		nineButton=new JButton("9");
 		nineButton.setBounds(230, 130, 80, 80);
 		nineButton.setFont(new Font("Areal", Font.PLAIN, 40));
-		sevenButton.addActionListener(this);
+		nineButton.addActionListener(this);
 		jf.add(nineButton);
 		fourButton=new JButton("4");
 		fourButton.setBounds(30, 230, 80, 80);
 		fourButton.setFont(new Font("Areal", Font.PLAIN, 40));
-		sevenButton.addActionListener(this);
+		fourButton.addActionListener(this);
 		jf.add(fourButton);
 		fiveButton=new JButton("5");
 		fiveButton.setBounds(130, 230, 80, 80);
 		fiveButton.setFont(new Font("Areal", Font.PLAIN, 40));
-		sevenButton.addActionListener(this);
+		fiveButton.addActionListener(this);
 		jf.add(fiveButton);
 		sixButton=new JButton("6");
 		sixButton.setBounds(230, 230, 80, 80);
 		sixButton.setFont(new Font("Areal", Font.PLAIN, 40));
-		sevenButton.addActionListener(this);
+		sixButton.addActionListener(this);
 		jf.add(sixButton);
 		oneButton=new JButton("1");
 		oneButton.setBounds(30, 330, 80, 80);
 		oneButton.setFont(new Font("Areal", Font.PLAIN, 40));
-		sevenButton.addActionListener(this);
+		oneButton.addActionListener(this);
 		jf.add(oneButton);
 		twoButton=new JButton("2");
 		twoButton.setBounds(130, 330, 80, 80);
 		twoButton.setFont(new Font("Areal", Font.PLAIN, 40));
-		sevenButton.addActionListener(this);
+		twoButton.addActionListener(this);
 		jf.add(twoButton);
 		threeButton=new JButton("3");
 		threeButton.setBounds(230, 330, 80, 80);
 		threeButton.setFont(new Font("Areal", Font.PLAIN, 40));
-		sevenButton.addActionListener(this);
+		threeButton.addActionListener(this);
 		jf.add(threeButton);
 		dotButton=new JButton(".");
 		dotButton.setBounds(30, 430, 80, 80);
 		dotButton.setFont(new Font("Areal", Font.PLAIN, 40));
-		sevenButton.addActionListener(this);
+		dotButton.addActionListener(this);
 		jf.add(dotButton);
 		zeroButton=new JButton("0");
 		zeroButton.setBounds(130, 430, 80, 80);
 		zeroButton.setFont(new Font("Areal", Font.PLAIN, 40));
-		sevenButton.addActionListener(this);
+		zeroButton.addActionListener(this);
 		jf.add(zeroButton);
 		equalButton=new JButton("=");
 		equalButton.setBounds(230, 430, 80, 80);
 		equalButton.setFont(new Font("Areal", Font.PLAIN, 40));
-		sevenButton.addActionListener(this);
+		equalButton.addActionListener(this);
 		jf.add(equalButton);
 		divButton=new JButton("/");
 		divButton.setBounds(330, 130, 80, 80);
 		divButton.setFont(new Font("Areal", Font.PLAIN, 40));
-		sevenButton.addActionListener(this);
+		divButton.addActionListener(this);
 		jf.add(divButton);
 		mulButton=new JButton("x");
 		mulButton.setBounds(330, 230, 80, 80);
 		mulButton.setFont(new Font("Areal", Font.PLAIN, 40));
-		sevenButton.addActionListener(this);
+		mulButton.addActionListener(this);
 		jf.add(mulButton);
 		subButton=new JButton("-");
 		subButton.setBounds(330, 330, 80, 80);
 		subButton.setFont(new Font("Areal", Font.PLAIN, 40));
-		sevenButton.addActionListener(this);
+		subButton.addActionListener(this);
 		jf.add(subButton);
 		addButton=new JButton("+");
 		addButton.setBounds(330, 430, 80, 80);
 		addButton.setFont(new Font("Areal", Font.PLAIN, 40));
-		sevenButton.addActionListener(this);
+		addButton.addActionListener(this);
 		jf.add(addButton);
 		jf.setVisible(true);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -134,39 +134,51 @@ public class Calculator implements ActionListener
 	{
 		if(e.getSource()==sevenButton)
 		{
-			displayLabel.setText("7");
+			displayLabel.setText(displayLabel.getText()+"7");
 		}
 		else if(e.getSource()==eightButton)
 		{
-			displayLabel.setText("8");
+			displayLabel.setText(displayLabel.getText()+"8");
 		}
 		else if(e.getSource()==nineButton)
 		{
-			displayLabel.setText("9");
+			displayLabel.setText(displayLabel.getText()+"9");
 		}
 		else if(e.getSource()==fourButton)
 		{
-			displayLabel.setText("4");
+			displayLabel.setText(displayLabel.getText()+"4");
 		}
 		else if(e.getSource()==fiveButton)
 		{
-			displayLabel.setText("5");
+			displayLabel.setText(displayLabel.getText()+"5");
 		}
 		else if(e.getSource()==sixButton)
 		{
-			displayLabel.setText("6");
+			displayLabel.setText(displayLabel.getText()+"6");
 		}
 		else if(e.getSource()==oneButton)
 		{
-			displayLabel.setText("1");
+			displayLabel.setText(displayLabel.getText()+"1");
+		}
+		else if(e.getSource()==twoButton)
+		{
+			displayLabel.setText(displayLabel.getText()+"2");
+		}
+		else if(e.getSource()==threeButton)
+		{
+			displayLabel.setText(displayLabel.getText()+"3");
 		}
 		else if(e.getSource()==dotButton)
 		{
-			displayLabel.setText(".");
+			displayLabel.setText(displayLabel.getText()+".");
+		}
+		else if(e.getSource()==zeroButton)
+		{
+			displayLabel.setText(displayLabel.getText()+"0");
 		}
 		else
 		{
-			displayLabel.setText("0");
+			displayLabel.setText("Invalid");
 		}
 	}
 }
